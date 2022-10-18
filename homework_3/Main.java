5b53ce35cc3d68d6ab01a2c6b49d7ecc368eefda
Eugene Kolenko <kolenko.evg@gmail.com>
2022-10-18 18:40:13 +0600

    Свинья обновлённая

--- homework_3/Main.java
+++ homework_3/Main.java
@@ -9,7 +9,7 @@ public class Main {
     
     public static void main(String[] args) throws IOException { 
 
-        var file = new FileReader("file.json");
+        var file = new FileReader("homework_3/file.json");
 
         char[] buf = new char[256];
             
@@ -34,7 +34,7 @@ public class Main {
         }    
         
         for (Pig pig : pigList) {
-            
+
             System.out.println(pig);
             
         }
